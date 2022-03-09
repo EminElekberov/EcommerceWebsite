6b2fc5dbfdb0e1b474e9f733749624d5fc8a2212
EminElekberov <eminelekberov09@gmail.com>
2022-03-09 23:04:16 +0400

    update

---
+++ aspPortoWebsite/Areas/Admin/Controllers/RoleController.cs
@@ -0,0 +1,79 @@
+﻿using aspPortoWebsite.Models;
+using aspPortoWebsite.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aspPortoWebsite.Areas.Admin.Controllers
+{
+    public class RoleController : Controller
+    {
+        private readonly PortoDbContext _portoDbContext;
+        private readonly UserManager<User> _userManager;//bunun vasitesile gedib usrin mailini tapir,dogrulugunu yoxlayir.Check ve CRUD emeliyyatlari
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public RoleController(PortoDbContext portoDbContext, UserManager<User> userManager, RoleManager<IdentityRole> roleManager, SignInManager<User> signInManager)
+        {
+            _portoDbContext = portoDbContext;
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _signInManager = signInManager;
+        }
+        public IActionResult Index()
+        {
+            var group = _portoDbContext.UserRoles.Include(x => x.UserId).Include(d => d.RoleId).ToList();
+            return View(group);
+        }
+        [HttpGet]
+        public async Task<IActionResult> ManageUserRoles(string id)
+        {
+            ViewBag.userid = id;
+            var user = await _userManager.FindByIdAsync(id);
+            if (user==null)
+            {
+                return NotFound();
+            }
+            var model = new List<UserRoleViewModel>();
+            foreach (var item in _roleManager.Roles)
+            {
+                var userRoles = new UserRoleViewModel
+                {
+                    RoleId = item.Id,
+                    RoleName=item.Name
+                };
+                if (await _userManager.IsInRoleAsync(user,item.Name))
+                {
+                    userRoles.IsSelected = true;
+                }
+                else
+                {
+                    userRoles.IsSelected = false;
+                }
+                model.Add(userRoles);
+            }
+            return View(model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> ManageUserRoles(List<UserRoleViewModel> models,string userId)
+        {
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user==null)
+            {
+                return NotFound();
+            }
+            var role = await _userManager.GetRolesAsync(user);
+            var result = await _userManager.RemoveFromRolesAsync(user, role);
+            if (!result.Succeeded)
+            {
+                return NotFound();
+            }
+           //result = await _userManager.AddToRoleAsync(user, models.Where(x => x.IsSelected).Select(y => y.RoleName));
+            return Redirect("/Admin/Slider/Index");
+        }
+    }
+}
