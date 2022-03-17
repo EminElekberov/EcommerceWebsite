6dcad6384edbfa677925adb2eb7e6abaf373d80b
EminElekberov <eminelekberov09@gmail.com>
2022-03-17 19:55:20 +0400

    update

---
+++ aspPortoWebsite/Areas/Admin/Controllers/GroupController.cs
@@ -0,0 +1,24 @@
+﻿using aspPortoWebsite.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aspPortoWebsite.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class GroupController : Controller
+    {
+        private readonly PortoDbContext dbContext;
+        public GroupController(PortoDbContext _dbContext)
+        {
+            dbContext = _dbContext;
+        }
+        public IActionResult Index()
+        {
+            return View(dbContext.Groups.ToList());
+        }
+        
+    }
+}
