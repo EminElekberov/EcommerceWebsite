68caab4fb824f2a52cbb25f6ea59b3bcbcabb092
EminElekberov <eminelekberov09@gmail.com>
2022-04-05 23:17:18 +0400

    update

--- aspPortoWebsite/Models/Group.cs
+++ aspPortoWebsite/Models/Group.cs
@@ -7,10 +7,10 @@ namespace aspPortoWebsite.Models
 {
     public class Group
     {
-
         public int Id { get; set; }
         public string Fullname { get; set; }
         public virtual ICollection<Student> Students { get; set; }
+
 
     }
 }
