65084984c83dcdad0340edea93b8e0653a8eee89
EminElekberov <eminelekberov09@gmail.com>
2022-05-01 23:23:47 +0400

    update

--- aspPortoWebsite/Models/PortoDbContext.cs
+++ aspPortoWebsite/Models/PortoDbContext.cs
@@ -9,8 +9,8 @@ namespace aspPortoWebsite.Models
 {
     public class PortoDbContext:IdentityDbContext<User>
     {
-
         public PortoDbContext(DbContextOptions<PortoDbContext> options):base(options) { }
+
         public DbSet<Category> Categories { get; set; }
         public DbSet<Slider> Sliders { get; set; }
         public DbSet<Student> Students { get; set; }
