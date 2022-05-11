cd17997ab699f62ac4c5d68baa5280aaeb2c09e4
EminElekberov <eminelekberov09@gmail.com>
2022-05-11 22:41:29 +0400

    update

--- aspPortoWebsite/Controllers/FilterController.cs
+++ aspPortoWebsite/Controllers/FilterController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using aspPortoWebsite.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,13 +10,32 @@ namespace aspPortoWebsite.Controllers
 {
     public class FilterController : Controller
     {
-        public IActionResult Index(int rangePrimary,int size)
+        private readonly PortoDbContext portoDbContext;
+        public FilterController(PortoDbContext portoDb)
         {
+            portoDbContext = portoDb;
+        }
+        public async Task<IActionResult> Index(int rangePrimary, int size)
+        {
+            List<SizeToBooks> sizes=new List<SizeToBooks>();
+            //List<SizeToBooks> sizes;
             if (rangePrimary == null)
             {
                 return NotFound();
             }
-            return View();
+            if (rangePrimary!=0 && size==0)
+            {
+                sizes= portoDbContext.SizeToBooks.Include(x => x.Books).Include(x => x.Size).Where(x =>x.Books.PresentPrice <= rangePrimary).ToList();
+            }
+            else if(rangePrimary!=null &&  size!=0)
+            {
+                sizes = portoDbContext.SizeToBooks.Include(x => x.Books).Include(x => x.Size).Where(x => x.SizeId == size && x.Books.PresentPrice <= rangePrimary).ToList();
+            }
+            else
+            {
+                return RedirectToAction("/Home/Index");
+            }
+            return View(sizes);
         }
     }
 }
