065d650e36e2ab47ad5bee082ad64a8aa7d54d24
EminElekberov <eminelekberov09@gmail.com>
2022-06-14 22:54:40 +0400

    update

--- aspPortoWebsite/ViewComponents/VcCart.cs
+++ aspPortoWebsite/ViewComponents/VcCart.cs
@@ -10,7 +10,6 @@ using System.Threading.Tasks;
 namespace aspPortoWebsite.ViewComponents
 {
     public class VcCart : ViewComponent
-
     {
         private readonly PortoDbContext dbContext;
         public VcCart(PortoDbContext _dbcontext)
