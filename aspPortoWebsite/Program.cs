94483c5af0d4ee4c5447377966174099e4f5913f
EminElekberov <eminelekberov09@gmail.com>
2022-09-27 20:26:50 +0400

    update

--- aspPortoWebsite/Program.cs
+++ aspPortoWebsite/Program.cs
@@ -15,6 +15,7 @@ namespace aspPortoWebsite
         {
             CreateHostBuilder(args).Build().Run();
         }
+
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
